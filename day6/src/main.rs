96fe777d163ca6555da5e90c4b0a3a8866ea7be8
Fermi-4 <fermiparadox4@gmail.com>
2022-12-06 01:01:18 -0600

    day 6 part 1 done

---
+++ day6/src/main.rs
@@ -0,0 +1,26 @@
+
+use std::collections::HashSet;
+use std::fs;
+
+fn main() {
+    let filename = "real.txt";
+    let window_size = 4;
+    let rx_buffer: String = get_data(&filename);
+    println!("{:?}", rx_buffer);
+    for i in 0..rx_buffer.len()-window_size+1 {
+        let slice = &rx_buffer[i..window_size+i];
+        println!("slice: {}", slice);
+        let mut hash: HashSet<char> = HashSet::new();
+        let is_not_signature: bool = slice.chars()
+                               .map(|c| hash.insert(c))
+                               .any(|r| !r);
+        if !is_not_signature {
+            println!("The answer is: {}", i+window_size);
+            break;
+        }
+    };
+}
+
+fn get_data(filename: &str) -> String {
+    fs::read_to_string(filename).unwrap()
+}
