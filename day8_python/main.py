9722ff84628876b953c569819d409b41bbfb2905
Fermi-4 <fermiparadox4@gmail.com>
2022-12-12 20:30:13 -0600

    day 8 part 2 done

--- day8_python/main.py
+++ day8_python/main.py
@@ -28,6 +28,60 @@ def get_val(n,r,c):
         return 1
     else:
         return 0
+
+
+
+
+def get_score(n, r, c):
+    """Get viewing distance in all dirs and multiply together"""
+    val=n[r][c]
+    rows=len(n)
+    cols=len(n[0])
+    lc=0
+    rc=0
+    tc=0
+    bc=0
+    # left
+    print(n[r])
+    print(val)
+    print('lc----')
+    for i in reversed(range(0,c)):
+        print("%i - %i" % (i, n[r][i]))
+        lc+=1
+        if n[r][i] >= val:
+            print('breaking on [%i]' % n[i][c])
+            break
+    print('lc=%i----' % lc)
+    
+    print('rc----')
+    for i in range(c+1,cols):
+        print("%i - %i" % (i, n[r][i]))
+        rc+=1
+        if n[r][i] >= val:
+            print('breaking on [%i]' % n[i][c])
+            break
+    print('rc=%i----' % rc)
+
+    print('tc----')
+    for i in reversed(range(0,r)):
+        print("%i - %i" % (i, n[i][c]))
+        tc+=1
+        if n[i][c] >= val:
+            print('breaking on [%i]' % n[i][c])
+            break
+    print('tc=%i----' % tc)
+
+    print('bc----')
+    for i in range(r+1, rows):
+        print("%i - %i" % (i, n[i][c]))
+        bc+=1
+        if n[i][c] >= val:
+            print('breaking on [%i]' % n[i][c])
+            break
+    print('bc=%i----' % bc)
+    return lc*rc*tc*bc 
+
+
 with open(PATH, "r") as file:
     data = file.read()
 print(data)
@@ -50,13 +104,21 @@ for r in range(0,rows):
             data[r][c]=1
         else:
             data[r][c]=get_val(numbers,r,c)
-    print(numbers[r])
-    print(data[r])
 print("final data:")
 print(data)
 print(data.sum(axis=1).sum())
 
-
-
+# part 2
+data=np.zeros((rows,cols), dtype=int)
+for r in range(0,rows):
+    print("processing row: [%i]" % r)
+    for c in range(0,cols):
+        data[r][c]=get_score(numbers, r, c)
+print("final data:")
+print(data)
+print(numbers)
+print(data.max(axis=1).max())
+print(rows)
+print(cols)
 
 
